503b904ad221ceaca2f3854652c3f7cd9dc34f5b
Shashank <shashank.keshava@bookmyshow.com>
2017-11-15 19:02:55 +0530

    Lecture 10

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+const App = function() {
+	return <div>Hi</div>;
+};
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+ReactDom.render(<App/>);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
