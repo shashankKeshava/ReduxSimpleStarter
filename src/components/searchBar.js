5bb6f9ae685b3e9fe82a10fa6ff10686cd7b00ef
Shashank <shashank.keshava@bookmyshow.com>
2017-11-16 18:02:34 +0530

    Lecture 22

---
+++ src/components/searchBar.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+//State based component
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: ""
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<input
+					value={this.state.value}
+					onChange={e=>this.setState({term:e.target.value})}
+				/>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDom from "react-dom";
+import YTSearch from "youtube-api-search";
 
-const App = function() {
-	return <div>Hi</div>;
-};
+import SearchBar from "./components/searchBar.js";
+const API_KEY = "AIzaSyBMHQdixQTMkGzVpxzlNTDzeHPDr9a3hRg";
 
-ReactDom.render(<App/>);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: []
+		};
 
+		YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+			this.setState({videos});
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
+
+ReactDom.render(<App />, document.querySelector(".container"));
