ca228ba9f32e87f48b6168fbedce94a12f41f785
Shashank <shashank.keshava@bookmyshow.com>
2017-11-21 17:32:59 +0530

    minor CHanges

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -2,10 +2,10 @@ import React from "react";
 
 import VideoListItem from "./videoListItem.js";
 const videoList = props => {
-	props.videos.map(video => {
-		return <VideoListItem video={video} />;
+	const videoItems = props.videos.map(video => {
+		return <VideoListItem key={video.etag} video={video} />;
 	});
-	return <ul className="list-group">{videoList}</ul>;
+	return <ul className="list-group">{videoItems}</ul>;
 };
 
 export default videoList;
