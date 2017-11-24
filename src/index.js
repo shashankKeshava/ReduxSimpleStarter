b78a225d05fdb4999b8f7563c2c8e567fb6372a4
Shashank <shashank.keshava@bookmyshow.com>
2017-11-24 14:08:55 +0530

    lecture 33 Wrap Up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
 import YTSearch from "youtube-api-search";
+import _ from "lodash";
 
 import SearchBar from "./components/searchBar.js";
 import VideoList from "./components/videoList.js";
@@ -25,9 +26,10 @@ class App extends Component {
 		});
 	};
 	render() {
+		const videoSearch=_.debounce((term)=>{this._videoSearch(term)},300);
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term=>this._videoSearch(term)}/>
+				<SearchBar onSearchTermChange={videoSearch}/>
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList
 					onVideoSelect={selectedVideo =>
